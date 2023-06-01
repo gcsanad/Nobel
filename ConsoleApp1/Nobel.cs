87098e7bd961777ae2c8de94775819de9f653a10
gcsanad <71721197+gcsanad@users.noreply.github.com>
2023-06-01 11:08:53 +0200

    kész

---
+++ ConsoleApp1/Nobel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Nobel
+    {
+        int evszam;
+        string tipus;
+        string keresztnev;
+        string vezeteknev;
+
+
+        public Nobel(string sor)
+        {
+            string[] felosztas = sor.Split(';');
+            this.evszam = int.Parse(felosztas[0]);
+            this.tipus = felosztas[1];
+            this.keresztnev = felosztas[2];
+            this.vezeteknev = felosztas[3];
+        }
+
+        public int Evszam { get => evszam; }
+        public string Tipus { get => tipus;}
+        public string Keresztnev { get => keresztnev;}
+        public string Vezeteknev { get => vezeteknev;}
+
+        
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,45 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //2.Feladat
+            List<Nobel> lista = new List<Nobel>(File.ReadAllLines("C:/GCs/ConsoleApp1/ConsoleApp1/nobel.csv").Skip(1).Select(x => new Nobel(x)).ToList());
+
+            //4.Feladat
+            List<Nobel> irodalmiDijak = lista.Where(x => x.Tipus == "irodalmi" && x.Evszam == 2017).ToList();
+            foreach (var item in irodalmiDijak)
+            {
+                Console.WriteLine($"4.Feladat: {item.Keresztnev} {item.Vezeteknev}");
+            }
+
+
+            //6.Feladat
+            List<Nobel> curieTagok = lista.Where(x => x.Vezeteknev.Contains("Curie")).ToList();
+            Console.WriteLine("6.Feladat:");
+            foreach (var item in curieTagok)
+            {
+                Console.WriteLine($"\t{item.Evszam}:{item.Keresztnev} {item.Vezeteknev} ({item.Tipus})");
+            }
+
+            //7.Feladat
+            Console.WriteLine("7.Feladat");
+            Console.WriteLine($"\tfizikai                            {lista.Count(x => x.Tipus=="fizikai")} db");
+            Console.WriteLine($"\tkémiai                             {lista.Count(x => x.Tipus == "kémiai")} db");
+            Console.WriteLine($"\torvosi                             {lista.Count(x => x.Tipus == "orvosi")} db");
+            Console.WriteLine($"\tirodalmi:                          {lista.Count(x => x.Tipus == "irodalmi")} db");
+            Console.WriteLine($"\tbéke                               {lista.Count(x => x.Tipus == "béke")} db");
+            Console.WriteLine($"\tközgazdaságtani                     {lista.Count(x => x.Tipus == "közgazdaságtani")} db");
+
+            //8.Feladat
+            StreamWriter sr = new StreamWriter("orvosi.txt");
+            foreach (var item in lista)
+            {
+                if (item.Tipus == "orvosi")
+                {
+                    sr.WriteLine($"{item.Evszam}:{item.Keresztnev} {item.Vezeteknev}");
+                }
+            }
+            Console.WriteLine("Sikeres mentés!");
+
         }
     }
 }
